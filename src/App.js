c6ef8c49da317f8e4c6546744de14304ecc75596
devilla <dev.koold@gmail.com>
2024-02-19 04:31:51 +0530

    Remove SineWave component and adjust canvas size

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import CrashGame from './Components/CrashGame/CrashGame';
-import SineWave from './Components/SineWave/SineWave';
 import ExponentialGraphCanvas from './Components/ExponentialGraphCanvas/ExponentialGraphCanvas';
 
 function App() {
   return (
     <div className="App">
-      {/* <SineWave /> */}
       <ExponentialGraphCanvas />
       <footer className="App-footer">
         <CrashGame />
--- src/Components/ExponentialGraphCanvas/ExponentialGraphCanvas.js
+++ src/Components/ExponentialGraphCanvas/ExponentialGraphCanvas.js
@@ -46,7 +46,9 @@ const ExponentialGraphCanvas = () => {
     ctx.stroke();
   }, []);
 
-  return <canvas ref={canvasRef} width={400} height={200} />;
+  const maxWidth = window.innerWidth; // Set the maximum width for the canvas to the screen width
+  const innerHeight = window.innerHeight; // Set the height of the canvas to the screen height
+  return <canvas ref={canvasRef} width={Math.min(maxWidth, window.innerWidth)} height={innerHeight} />;
 };
 
 export default ExponentialGraphCanvas;
