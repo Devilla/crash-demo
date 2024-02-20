75d87e19890adb4f84ece565be3457d4d5e12ea2
devilla <dev.koold@gmail.com>
2024-02-20 13:47:55 +0530

    Update Jest configuration and remove unused test file

--- src/tests/CrashGameTest.js
+++ src/tests/CrashGameTest.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import CrashGame from './CrashGame';
+import CrashGame from './CrashGameTest';
 
 describe('CrashGame', () => {
   test('renders CrashGame component', () => {
